c99cffcf4b8ebc602eabad5ac2f1ea9041932895
Mark Kolb <99706832+TheToasterRoaster@users.noreply.github.com>
2022-03-16 18:01:23 -0400

    done

--- src/main/java/com/room3/Driver.java
+++ src/main/java/com/room3/Driver.java
@@ -4,53 +4,69 @@ import java.lang.reflect.InvocationTargetException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import com.room3.dao.DaoImpl;
-
 
 import com.room3.models.Crime;
 import com.room3.models.SuperPrison;
 import com.room3.models.SuperVillain;
 import com.room3.util.Configuration;
 
-
 public class Driver {
 
-
 	static DaoImpl cdao = new DaoImpl();
-//	static SuperPrisonDao spdao = new SuperPrisonDao();
-//	static SuperVillainDao svdao = new SuperVillainDao();
+	static Scanner scan = new Scanner(System.in);
 
-	
-	public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchFieldException, SecurityException, SQLException  {
-		
-		System.out.println("Running project demo Demo");
+	public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException,
+			InvocationTargetException, InstantiationException, NoSuchFieldException, SecurityException, SQLException {
 		
 		Configuration setStuff = new Configuration("C:\\Users\\mkolb\\Desktop\\New folder\\Project1SV\\src\\main\\resources\\application.properties");
+		scan.nextLine();
 		
+		System.out.println("Running project demo Demo");
+		//Creates all Tables from a package
+		cdao.findAllClasses("com.room3.models");
 		
-		Crime c1 = new Crime(1,"Freezy", "covering the city in ice");
-//		Crime c2 = new Crime("Time Manipulation", "freezin' time, robbin'banks");
-//		Crime c3 = new Crime("Arson", "setting the city ablaze");
-//		cdao.findAllClasses("com.room3.models");
-//		cdao.insert(c1);
-//		cdao.insert(c2);
-//		cdao.insert(c3);
-//		
-		
-		
-		SuperPrison arkham = new SuperPrison("Arkham Asylum", "Gotham City");
-		
-		SuperVillain joker = new SuperVillain(1,"The Joker", "evilness", 600000);
-	
-		//cdao.insert(arkham);
-		//cdao.selectById(joker,1);
-		List<Object> crimes = cdao.selectAllByValueInColumn("evilness", "superpower", SuperVillain.class);
-		System.out.println(crimes.toString());
-		//cdao.deleteById(c1, 2);
+		scan.nextLine();		
 
+		Crime c1 = new Crime("Freezy", "covering the city in ice");
+		Crime c2 = new Crime("Time Manipulation", "freezin' time, robbin'banks");
+		Crime c3 = new Crime("Arson", "setting the city ablaze");
+		Crime c4 = new Crime("Arson", "Fire and Fury");
+		SuperPrison arkham = new SuperPrison("Arkham Asylum", "Gotham City");
+		SuperVillain joker = new SuperVillain("The Joker", "evilness", 600000);
 		
-		System.out.println(cdao.updateSingle(joker).toString());
+		cdao.insert(c4);
+		cdao.insert(c1);
+		cdao.insert(c2);
+		cdao.insert(c3);
+		cdao.insert(joker);
+		cdao.insert(arkham);
+		
+		scan.nextLine();
+		
+		Crime updateCrime = (Crime) cdao.selectById(c3, 3);
+		System.out.println(updateCrime);
+		
+		scan.nextLine();
+		
+		
+		List<Object> crimes = cdao.selectAllByValueInColumn("Arson", "crime_name", Crime.class);
+		System.out.println(crimes.toString());
+		
+		scan.nextLine();
+	    cdao.deleteById(c1, 2);
+		
+		updateCrime.setCrimeName("robbery");
+		updateCrime.setDescription("take stuff");
+		System.out.println(cdao.updateSingle(updateCrime).toString());
+		
+		scan.nextLine();
+		
+		System.out.println(cdao.findAll(c1));
+		
+		cdao.deleteById(c1, 1);
 	}
 
 }
